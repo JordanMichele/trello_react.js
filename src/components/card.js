d86cf47117be1715d07b0b68079f81a2e69ea8af
Jordan Michele <jordanmichele32@gmail.com>
2019-02-10 13:03:25 -0500

    added copy to card.js

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './card.css';
 
 export default function Card() {
-    const text  = 'Example card';
+    const text  = 'Caleb Raye';
     return (
         <div className="card">
             {text}
