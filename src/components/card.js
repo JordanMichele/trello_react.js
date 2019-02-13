85d04d5229091dea53d2a9bc58ea86aa1eedb7e1
Jordan Michele <jordanmichele32@gmail.com>
2019-02-12 21:21:02 -0500

    added dynamic functionality to the card component

--- src/components/card.js
+++ src/components/card.js
@@ -3,10 +3,10 @@ import React from 'react';
 import './card.css';
 
 export default function Card() {
-    const text  = 'Caleb Raye';
+    const text  = ['Jordan', 'Erin', 'Sean', 'Casey', 'Whitney', 'Linda', 'Mark', 'Brian'];
     return (
         <div className="card">
-            {text}
+            {text[Math.floor(Math.random() * text.length)]}
         </div>
     );
 };
